f67da713ee2b1540623e088065cfcd2e7971a72e
Daniel Levy <daniell@aptivate.org>
2015-06-03 16:55:08 +0100

    Change settings to work with Django 1.8

--- tests/settings.py
+++ tests/settings.py
@@ -4,12 +4,14 @@ USE_TZ = True
 
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
 
-RUN_WITH_ORIGIN = os.environ.get('RUN_WITH_ORIGIN', False) # comes from tox.ini
+RUN_WITH_ORIGIN = os.environ.get('RUN_WITH_ORIGIN', False)  # comes from tox.ini
 
 if RUN_WITH_ORIGIN:
     INSTALLED_APPS = ('actionitems', 'tests')
-    ACTIONITEMS_ORIGIN_MODEL='tests.TestModel'
+    ACTIONITEMS_ORIGIN_MODEL = 'tests.TestModel'
 else:
-    INSTALLED_APPS = ('actionitems')
+    INSTALLED_APPS = ('actionitems',)
 
 ROOT_URLCONF = 'actionitems.urls'
+
+SECRET_KEY = 'ajunksecretkeythatdoesntsecureanything'
